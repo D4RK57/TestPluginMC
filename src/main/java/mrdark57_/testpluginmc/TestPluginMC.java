1d03ae47b53c2faeae02ab16e29e8a032e745d74
DanRuizK57 <d.ruizk03@ufromail.cl>
2023-05-07 17:00:34 -0400

    Config añadida

--- src/main/java/mrdark57_/testpluginmc/TestPluginMC.java
+++ src/main/java/mrdark57_/testpluginmc/TestPluginMC.java
@@ -2,32 +2,38 @@ package mrdark57_.testpluginmc;
 
 import mrdark57_.testpluginmc.commands.Kit;
 import mrdark57_.testpluginmc.events.PlayerJoinMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class TestPluginMC extends JavaPlugin {
 
-    private String pluginName ="[" + getName() + "]";
+    private String pluginName = ChatColor.YELLOW + "[" + getName() + "]";
     private String pluginVersion ="v" + getDescription().getVersion();
+
+    private String configRoute;
 
     @Override
     public void onEnable() {
         enableMessage();
-
         commandRegister();
-
         eventRegister();
+        configRegister();
     }
     @Override
     public void onDisable() {
-        getLogger().info("onDisable is called!");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "----------------------------------------");
+        Bukkit.getConsoleSender().sendMessage(pluginName + ChatColor.RED + " has been disabled");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "----------------------------------------" + "\n");
     }
 
     // Enviar mensaje cuando se inicia el plugin
     public void enableMessage() {
-        getLogger().info("----------------------------------------");
-        getLogger().info(pluginName + " has been enabled (" + pluginVersion + ")");
-        getLogger().info("----------------------------------------");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "----------------------------------------");
+        Bukkit.getConsoleSender().sendMessage(pluginName + ChatColor.AQUA + " has been enabled" + ChatColor.RED +  " (" + pluginVersion + ")");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "----------------------------------------" + "\n");
     }
 
     // Registrar comandos
@@ -39,7 +45,21 @@ public class TestPluginMC extends JavaPlugin {
     // Registrar eventos
     public void eventRegister() {
         // Registrar evento
-        getServer().getPluginManager().registerEvents(new PlayerJoinMessage(), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoinMessage(this), this);
     }
 
+    // Registrar la config
+    public void configRegister() {
+
+        File config = new File(this.getDataFolder(),"config.yml");
+        configRoute = config.getPath();
+        if (!config.exists()){
+            this.getConfig().options().copyDefaults(true);
+            saveConfig();
+        }
+    }
+
+    public String getConfigRoute() {
+        return configRoute;
+    }
 }
--- src/main/java/mrdark57_/testpluginmc/events/PlayerJoinMessage.java
+++ src/main/java/mrdark57_/testpluginmc/events/PlayerJoinMessage.java
@@ -1,6 +1,10 @@
 package mrdark57_.testpluginmc.events;
 
+import mrdark57_.testpluginmc.TestPluginMC;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,9 +12,23 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoinMessage implements Listener {
 
     // Cuando ocurre el evento PlayerJoinEvent (jugador entra al servidor) envía un mensaje
+
+    private TestPluginMC plugin;
+
+    public PlayerJoinMessage(TestPluginMC plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Bukkit.broadcastMessage("Welcome " + event.getPlayer().getName() + " to the server!");
+
+        Player player = event.getPlayer();
+        FileConfiguration config = plugin.getConfig();
+        String path = "Config.welcome-msg";
+
+        if (config.getString(path).equals("true")){
+            player.sendMessage(ChatColor.AQUA + "Welcome " + event.getPlayer().getName() + " to the server!");
+        }
     }
 
 }
